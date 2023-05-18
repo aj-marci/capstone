4f47440cab72a9d2101c453eec9d19cc91f24200
aj-marci <abmarcinek@gmail.com>
2023-05-18 11:44:54 -0500

    updated responsive footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,43 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
-import "./App.css";
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-const footerItems = [
+function Footer () {
+  return (
+    <>
+    <footer className='fixed-bottom'>
+    <Card variant="light" bg="light" border="light">
+      <Row>
+      <Col sm={12} lg={6}>
+        <Card.Body>
+          <Card.Title>Contact Us</Card.Title>
+          <Card.Link href="mailto: help@littlelemon.com">
+            <FontAwesomeIcon icon={faEnvelope} size="xl" color="darkgrey"/>
+          </Card.Link>
+        </Card.Body>
+        </Col>
+        <Col sm={12} lg={6}>
+        <Card.Body>
+          <Card.Title>Social Media</Card.Title>
+          <Card.Link href="https://meta.com" target="_blank">
+            <FontAwesomeIcon icon={faFacebookF} size="xl" color="darkgrey"/>
+            </Card.Link>
+          <Card.Link href="https://instagram.com" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} size="2xl" color="darkgrey"/>
+          </Card.Link>
+        </Card.Body>
+        </Col>
+        </Row>
+    </Card>
+    </footer>
+    </>
+  );
+}
+
+export default Footer;
+
+/*const footerItems = [
   {
     icon: faEnvelope,
     url: "mailto: help@littlelemon.com",
@@ -21,7 +54,7 @@ function Footer () {
   return (
     <>
     <footer className='fixed-bottom'>
-    <Row sm={1} md={1} lg={2} className="g-4" style={{width: "12%"}}>
+    <Row sm={1} lg={1} className="g-4" style={{width: "12%"}}>
       {footerItems.map(({ icon, url }) => (
         <Col key={footerItems}>
           <Card
@@ -45,6 +78,4 @@ function Footer () {
     </footer>
     </>
   );
-}
-
-export default Footer;
+}*/
