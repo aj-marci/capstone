2d8d4d82087d1f368f4119aa8dea7b818454bbe3
aj-marci <abmarcinek@gmail.com>
2023-06-05 15:06:06 -0500

    added map to About section, started testing script to test if user is unable to submit without completing fields. testing script not working, not sure why.

--- src/About.js
+++ src/About.js
@@ -64,6 +64,17 @@ function About() {
               marginBottom:"-4rem"}}/>
             </Col>
             </Row>
+            <Row>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2967.414162603789!2d-87.6553327!3d41.9484384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd3b2e59adf21%3A0x1cea3ee176ddd646!2sWrigley%20Field!5e0!3m2!1sen!2sus!4v1685985410400!5m2!1sen!2sus"
+            width="400"
+            height="300"
+            style={{border:"0",
+            marginTop:"5rem",
+            marginBottom:"1rem"}}
+            allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            </Row>
         </Card>
         </article>
       </>
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,11 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import React from 'react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import ReserveMod1 from './ReserveMod1';
 
-describe("Reserve a Table Form", () => {
-  test("User is able to reserve a table at Little Lemon", () => {
+describe("Reserve Form", () => {
+  test("User is unable to submit form", () => {
     const handleSubmit = jest.fn();
     render(<ReserveMod1 onSubmit={handleSubmit} />);
-
-    const emailInput = screen.getByLabelText(/Your Email/);
-    fireEvent.change(emailInput, {target: {value:"ajaj@gmail.com"}});
-
-    const nameInput = screen.getByLabelText(/Your Name/);
-    fireEvent.change(nameInput, {target: {value:"Bob"}});
 
     const submitButton = screen.getByRole("button");
     fireEvent.click(submitButton);
@@ -18,4 +13,3 @@ describe("Reserve a Table Form", () => {
     expect(handleSubmit).not.toHaveBeenCalled();
   });
 });
-
